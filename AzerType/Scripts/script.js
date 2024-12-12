0ff73f1e8c4d30a2fdf36df67e7b2ec98c961492
RyanColy <ryan.coly999@gmail.com>
2024-12-12 01:03:42 +0000

    Script JS

---
+++ AzerType/Scripts/script.js
@@ -0,0 +1,39 @@
+function afficherResultat(score, nbMotsProposes){
+    console.log("Votre score est de " + score + " sur " + nbMotsProposes)
+}
+
+function choisirPhrasesouMots() {
+    let choix = prompt("Veuillez choisir une liste entre mots ou phrases")
+    while (choix !== "mots" && choix !== "phrases"){
+        choix = prompt("Veuillez choisir une liste entre mots ou phrases")
+    }
+    return choix
+}
+
+function lancerBoucleDeJeu(listePropositions) {
+    let score = 0
+    for (let i = 0; i < listePropositions.length; i++) {
+        let motUtilisateur = prompt("Entrez le mot " + listePropositions[i])
+            if (motUtilisateur === listePropositions[i]) {
+                score++
+        }
+    }
+    return score
+}
+
+function lancerJeu(){
+    let choix = choisirPhrasesouMots()
+    let score = 0
+    let nbMotsProposes = 0
+
+    if (choix === "mots"){
+        score = lancerBoucleDeJeu(listeMots)
+        nbMotsProposes = listeMots.length
+    }
+    else{
+        score = lancerBoucleDeJeu(listePhrases)
+        nbMotsProposes = listePhrases.length
+    }
+
+    afficherResultat(score, nbMotsProposes)
+}
